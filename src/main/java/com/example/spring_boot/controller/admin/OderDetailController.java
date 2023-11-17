8948994738856634b13302c0a971d39ddce41d49
kiennt <kien50626@gmail.com>
2023-11-18 05:32:24 +0700

    kien day code bill

--- src/main/java/com/example/spring_boot/controller/admin/OderDetailController.java
+++ src/main/java/com/example/spring_boot/controller/admin/OderDetailController.java
@@ -2,6 +2,7 @@ package com.example.spring_boot.controller.admin;
 
 import com.example.spring_boot.entity.OrderDetailEntity;
 import com.example.spring_boot.payload.request.OrderDetailRequest;
+import com.example.spring_boot.payload.request.ProductRequest;
 import com.example.spring_boot.payload.request.VoucherRequest;
 import com.example.spring_boot.service.OrderDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,4 +19,10 @@ import java.util.Optional;
 public class OderDetailController {
     @Autowired
     OrderDetailService orderDetailService;
+    @PostMapping("/delete")
+    public ResponseEntity<?> delete(@RequestBody OrderDetailRequest orderDetailRequest) {
+
+        return ResponseEntity.ok(orderDetailService.detele(orderDetailRequest));
+
+    }
 }
